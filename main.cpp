afe6ef8c4edcf8a3addbe48d09351a31159d788e
xuHaob1 <3218144994@qq.com>
2024-09-09 12:10:05 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 32181 on 2024/9/9.
+//
+
+#include <iostream>sssss
+int main(){
+    printf("hello world");
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by 32181 on 2024/9/9.
+//
+
+#ifndef GLUTTONOUS_SNAKE_MAIN_H
+#define GLUTTONOUS_SNAKE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //GLUTTONOUS_SNAKE_MAIN_H
