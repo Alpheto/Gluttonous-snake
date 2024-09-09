ca90fc64c5ca2204ed8835e8b8c01bb7237815b6
XU <3455272525@qq.com>
2024-09-09 12:42:00 +0800

    对于clion项目的第一次尝试修改

---
+++ cmake-build-debug/CMakeFiles/Gluttonous_snake.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gluttonous_snake.dir/main.cpp.obj"
+  "CMakeFiles/Gluttonous_snake.dir/main.cpp.obj.d"
+  "Gluttonous_snake.exe"
+  "Gluttonous_snake.exe.manifest"
+  "Gluttonous_snake.pdb"
+  "libGluttonous_snake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gluttonous_snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-//
-// Created by 32181 on 2024/9/9.
-//
-
-#include <iostream>sssss
-int main(){
-    printf("hello world");
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout<<"我"<<endl;
 }
